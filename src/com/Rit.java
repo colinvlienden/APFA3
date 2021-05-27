3fce8278ffda6875efa861c3c585461a9cc3bb4f
colinvlienden <colinvlienden@users.noreply.github.com>
2021-05-27 13:57:33 +0200

    add var distance + constructor, getter

--- src/com/Rit.java
+++ src/com/Rit.java
@@ -1,4 +1,14 @@
 package com;
 
-public class Rit {
+public class Rit extends Stap{
+    private Integer distance;
+
+    public Rit(String start, String end, Integer distance) {
+        super(start, end);
+        this.distance = distance;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
 }
