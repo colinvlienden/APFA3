c34eb21a3113a74f50c5eb5542eb7649ccd6f357
colinvlienden <colinvlienden@users.noreply.github.com>
2021-05-27 14:07:57 +0200

    Fill basic data, add print

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,5 +3,22 @@ package com;
 public class Main {
     public static void main(String [] args) {
 
+        //Treinrit
+        Treinrit T0 = new Treinrit("Culemborg", "Utrecht", 20);
+        Treinrit T1 = new Treinrit("Utrecht", "Amsterdam", 20);
+        System.out.println(T0);
+        System.out.println(T1);
+
+        //Rit
+        Rit R0 = new Rit("Culemborg", "Utrecht", 20);
+        Rit R1 = new Rit("Utrecht", "Amsterdam", 20);
+        System.out.println(R0);
+        System.out.println(R1);
+
+        //Vlucht
+        Vlucht V0 = new Vlucht("Culemborg", "Utrecht", 20);
+        Vlucht V1 = new Vlucht("Utrecht", "Amsterdam", 20);
+        System.out.println(V0);
+        System.out.println(V1);
     }
 }
