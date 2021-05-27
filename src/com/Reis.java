0ed408db28e30d8ff1abc0927880fbb35d8c5688
colinvlienden <colinvlienden@users.noreply.github.com>
2021-05-27 17:07:30 +0200

    Add Reis.java, compareTo werkt

--- src/com/Reis.java
+++ src/com/Reis.java
@@ -1,4 +1,36 @@
 package com;
 
-public class Reis {
+import java.util.PriorityQueue;
+
+public class Reis implements Comparable<Reis> {
+
+    private PriorityQueue<Stap> steps;
+
+    public void setSteps(PriorityQueue<Stap> steps) {
+        this.steps = steps;
+    }
+
+    private int getTotalWeight() {
+        return steps.stream().mapToInt(Stap::getWeight).sum();
+    }
+
+    @Override
+    public int compareTo(Reis reis) {
+        int reis1 = steps.stream().mapToInt(Stap::getWeight).sum();
+        int reis2 = reis.getTotalWeight();
+
+        if (reis1 < reis2){
+            System.out.println("Korter");
+            return 1;
+        }
+        else if(reis1 > reis2){
+            System.out.println("Groter");
+            return 1;
+        }
+        else {
+            System.out.println("Er is iets mis gegaan");
+            return 1;
+        }
+    }
+
 }
