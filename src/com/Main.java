46b5ecdeb707a41c26ea792bba76bb64cffa95e3
colinvlienden <colinvlienden@users.noreply.github.com>
2021-05-27 17:08:36 +0200

    Change test data, Create reis, add compareTo

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,34 +2,50 @@ package com;
 
 import java.util.PriorityQueue;
 
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String [] args) {
 
         //Treinrit
         Treinrit T0 = new Treinrit("Culemborg", "Utrecht", 20);
-        Treinrit T1 = new Treinrit("Utrecht", "Amsterdam", 20);
+        Treinrit T1 = new Treinrit("Utrecht", "Amsterdam", 30);
 
-        PriorityQueue<Stap> reisA = new PriorityQueue<>();
-        reisA.add(T0);
-        reisA.add(T1);
-        System.out.println(reisA);
+        PriorityQueue<Stap> Treinrit = new PriorityQueue<Stap>();
+        Treinrit.add(T0);
+        Treinrit.add(T1);
+        //System.out.println(Treinrit);
+
 
         //Rit
-        Rit R0 = new Rit("Culemborg", "Utrecht", 20);
-        Rit R1 = new Rit("Utrecht", "Amsterdam", 20);
+        Rit R0 = new Rit("Culemborg", "Utrecht", 30);
 
-        PriorityQueue<Stap> reisB = new PriorityQueue<>();
-        reisB.add(R0);
-        reisB.add(R1);
-        System.out.println(reisB);
+        PriorityQueue<Stap> Rit = new PriorityQueue<Stap>();
+        Rit.add(R0);
+
 
         //Vlucht
-        Vlucht V0 = new Vlucht("Culemborg", "Utrecht", 20, 2);
-        Vlucht V1 = new Vlucht("Utrecht", "Amsterdam", 20, 2);
+        Vlucht V0 = new Vlucht("Culemborg", "Utrecht", 90, 10);
 
-        PriorityQueue<Stap> reisC = new PriorityQueue<>();
-        reisC.add(V0);
-        reisC.add(V1);
-        System.out.println(reisC);
+        PriorityQueue<Stap> Vlucht = new PriorityQueue<Stap>();
+        Vlucht.add(V0);
+        //System.out.println(Vlucht);
+
+
+        //Create reis
+        Reis trein = new Reis();
+        trein.setSteps(Treinrit);
+
+        Reis auto = new Reis();
+        auto.setSteps(Rit);
+
+        Reis vliegtuig = new Reis();
+        vliegtuig.setSteps(Vlucht);
+
+        /* compare */
+        out.println(trein.compareTo(trein));
+
+        trein.compareTo(auto);
+        trein.compareTo(vliegtuig);
     }
 }
