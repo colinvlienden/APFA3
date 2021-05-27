f54368492f8f693d41e468430ba5044c9ea96267
colinvlienden <colinvlienden@users.noreply.github.com>
2021-05-27 16:22:06 +0200

    mke abstract, add abstract int getWeight()

--- src/com/Stap.java
+++ src/com/Stap.java
@@ -1,12 +1,14 @@
 package com;
 
 public abstract class Stap implements java.lang.Comparable<Stap> {
-    private String start;
-    private String end;
+    private final String start;
+    private final String end;
 
     public Stap(String start, String end) {
         this.start = start;
         this.end = end;
     }
+
+    public abstract int getWeight();
 
 }
