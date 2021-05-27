1ef1f86b80ef04260f0cfd8e006dd1a71f5d059f
colinvlienden <colinvlienden@users.noreply.github.com>
2021-05-27 13:38:47 +0200

    Add project, add start, end Stap.java

---
+++ src/com/Stap.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Stap {
+    private String start;
+    private String end;
+
+    public Stap(String start, String end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+}
