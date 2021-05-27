f1da29368a7e8c6725d8425b9cd78baf8f626b06
colinvlienden <colinvlienden@users.noreply.github.com>
2021-05-27 13:52:47 +0200

    Add extends Stap, add var duration

--- src/com/Treinrit.java
+++ src/com/Treinrit.java
@@ -1,5 +1,14 @@
 package com;
 
-public class Treinrit{
+public class Treinrit extends Stap{
+    private Integer duration;
 
+    public Treinrit(String start, String end, Integer duration) {
+        super(start, end);
+        this.duration = duration;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
 }
